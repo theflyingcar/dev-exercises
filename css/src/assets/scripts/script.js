9f562496929ec61d9e668c21386b6e85cc782a41
Jordan Smith <jordansmith@Jordans-MacBook-Pro.local>
2016-04-27 20:21:02 -0500

    Updates to CSS Exercise

--- css/src/assets/scripts/script.js
+++ css/src/assets/scripts/script.js
@@ -1,2 +1,13 @@
 // Javascript Document
 
+$('.vidbox-plus').click(function(e){
+	e.preventDefault();
+	// Call Function
+});
+
+// LIKE VIDEO
+$('.vidbox-like').click(function(e){
+	e.preventDefault();
+	$(this).toggleClass('vid_is-liked');
+	// Call Function
+});
