84527bb26e05c5f0c92a6fe69d553d49ac2b7f62
Jordan Smith <jordansmith@Jordans-MacBook-Pro.local>
2016-04-28 23:51:20 -0500

    Cleaned Comments from Example

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,21 +1,11 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
-
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
-if (input) {
-	return 'Hello, '+input+'!';
-} else {
-	return 'Hello, World!'
-}
-
+	if (input) {
+		return 'Hello, '+input+'!';
+	} else {
+		return 'Hello, World!'
+	}
 };
 
 module.exports = HelloWorld;
